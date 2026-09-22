66636f94f74aea0933e23902ece19f49974a179c
deepak-urs <gvdeepakurs2202@gmail.com>
2026-09-22 12:35:28 -0500

    hooks: useState (arrays)

--- react-js-tutorial/src/components/UseStates.tsx
+++ react-js-tutorial/src/components/UseStates.tsx
@@ -6,6 +6,9 @@ function UseStates() {
     const [productPrice, setProductPrice] = useState<number>(1200);
     const [isActive, setIsActive] = useState<boolean>(false);
 
+    const [cityList, setCityList] = useState<string[]>(['Pune', 'Mysuru']);
+    const [cityName, setCityName] = useState<string>('');
+
     const changeCourseName = () => {
         courseName = "Angular";
     }
@@ -22,9 +25,27 @@ function UseStates() {
         setIsActive(event.target.checked);
     }
 
+    const onCityNameChange = (event: any) => {
+        setCityName(event.target.value)
+    }
+
+    const addCity = () => {
+        setCityList(oldList => [...oldList, cityName]);
+    }
+
+    const addCities = () => {
+        setCityList(['a', 'b', 'c']);
+    }
+
     return <>
+        <p>{cityList}</p>
         <h1>Course Name: {courseName}</h1>
         <br></br>
+
+        <input type="text" placeholder="Enter City Name" onChange={(event) => (onCityNameChange(event))}/>
+        <button onClick={addCity}>Add City</button>
+        <button onClick={addCities}>Add New Cities</button>
+
         <h2>CourseVideo length: {courseVideos}</h2>
         <button onClick={changeCourseName}> Change Course Name</button>
         <button onClick={changeCourseVideos}> Change Course Video</button>
@@ -35,6 +56,9 @@ function UseStates() {
         isActive - {isActive ? 'checked' : 'unchecked'}
         <br></br> 
         <input type="checkbox" onChange={(event) => onActiveChange(event)} />
+        <br></br>
+
+
     </>
 }
 
