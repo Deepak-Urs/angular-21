abfdac919d30e88127494fbe21572ec2a2b5403a
deepak-urs <gvdeepakurs2202@gmail.com>
2026-09-23 15:42:49 -0500

    Router: basics

--- react-js-tutorial/src/App.tsx
+++ react-js-tutorial/src/App.tsx
@@ -1,19 +1,49 @@
+import { Routes, Route, Link } from 'react-router'
 import './App.css'
 import ConditionalRender from './components/ConditionalRender'
 import List from './components/List'
 import UseEffects from './components/UseEffects'
 import UseStates from './components/UseStates'
+import DataEventBinding from './components/DataEventBinding'
 
 
 function App() {
   return (
     <>
-      <h1>Hello React!</h1>
-      < List />
+
+
+    <nav className="navbar navbar-default">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <a className="navbar-brand" href="#">WebSiteName</a>
+        </div>
+        <ul className="nav navbar-nav">
+          <li><Link className="nav-link" to={'/'}> Home </Link></li>
+          <li><Link className="nav-link" to={'/data-event-binding'}> Data Binding</Link></li>
+          <li><Link className="nav-link" to={'/use-effects'}>Use Effects</Link></li>
+          <li><Link className="nav-link" to={'/use-states'}> Use States</Link></li>
+          <li><Link className="nav-link" to={'/conditional-render'}> Conditional Render</Link></li>
+          <li><Link className="nav-link" to={'/list'}> Lists</Link></li>
+          
+        </ul>
+      </div>
+    </nav>
+      
+      <Routes>
+        <Route path='/' element={<DataEventBinding/>}/>
+        <Route path='/data-event-binding' element={<DataEventBinding/>}/>
+        <Route path='/use-effect' element={<UseEffects/>}/>
+        <Route path='/use-state' element={<UseStates/>}/> 
+        <Route path='/conditional-render' element={<ConditionalRender/>}/>
+        <Route path='/list' element={<List/>}/>
+      </Routes>
       {/*<ConditionalRender />*/}
       
       {/*<UseEffects />*/}
       {/*<UseStates />*/}
+
+      {/*<h1>Hello React!</h1>
+      < List />*/}
     </>
   )
 }
